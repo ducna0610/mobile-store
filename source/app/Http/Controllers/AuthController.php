b0c91a66c383f392bca6d71abbd7099940a5d07f
ducna0610 <ducna0610@gmail.com>
2023-01-28 20:30:46 +0700

    👍 update(add AuthController)

---
+++ source/app/Http/Controllers/AuthController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\ProviderEnum;
+use App\Http\Requests\Admin\LoginAdminRequest;
+use App\Http\Requests\Admin\RegisterAdminRequest;
+use App\Http\Requests\User\LoginUserRequest;
+use App\Http\Requests\User\RegisterUserRequest;
+use App\Models\Admin;
+use App\Models\User;
+use DateTime;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+    // -- ADMIN --
+    public function adminLogin()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function adminRegister()
+    {
+        return view('admin.auth.register');
+    }
+
+    public function adminRegistering(RegisterAdminRequest $request)
+    {
+        $data = $request->validated();
+
+        $password = Hash::make(request()->password);
+
+        $data['password'] = $password;
+
+        $admin = new Admin();
+        $admin->fill($data);
+        $admin->save();
+
+        $admin = Admin::query()
+            ->where('email', '=', $request->email)
+            ->first(
+                [
+                    'name',
+                    'email',
+                    'id',
+                    'role',
+                ]
+            );
+
+        Auth::guard('admin')->login($admin);
+
+        return redirect()->route('admins.index');
+    }
+
+    public function adminLogging(LoginAdminRequest $request)
+    {
+        $data = $request->validated();
+
+        try {
+            $admin = Admin::query()
+                ->where('email', $request->get('email'))
+                ->firstOrFail();
+
+            if (!Hash::check($request->get('password'), $admin->password)) {
+                throw new Exception('Invalid password');
+            }
+
+            Auth::guard('admin')->login($admin);
+
+            return redirect()->route('admins.index');
+        } catch (\Throwable $e) {
+            return redirect()->route('admin-login');
+        }
+    }
+
+    public function adminLogout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        return redirect()->route('admin-login');
+    }
+
+    // -- USER --
+    public function registering(RegisterUserRequest $request)
+    {
+        $data = $request->validated();
+
+        $password = Hash::make($request->password);
+
+        $data['password'] = $password;
+
+        $user = User::create($data);
+
+        Auth::login($user);
+
+        return redirect()->route('homes.index')->with('success', 'Đăng ký thành công.');
+    }
+
+    public function callback($provider)
+    {
+        $data = Socialite::driver($provider)->user();
+
+        $provider_id = ProviderEnum::getKeyFromProviderEnum($provider);
+
+        $user = User::query()
+            ->firstOrCreate(
+                [
+                    'email' => $data->email,
+                    'provider' => $provider_id,
+                ],
+                [
+                    'name' => $data->name,
+                    'password' => Hash::make(rand()),
+                ]
+            );
+
+        Auth::login($user);
+
+        return redirect()->route('homes.index');
+    }
+
+
+    public function logging(LoginUserRequest $request)
+    {
+        try {
+            $user = User::query()
+                ->where('email', $request->get('email'))
+                ->firstOrFail();
+
+            if (!Hash::check($request->get('password'), $user->password)) {
+                throw new Exception('Invalid password');
+            }
+
+            Auth::login($user);
+
+            return redirect()->route('homes.index');
+        } catch (\Throwable $e) {
+            // throw $e;
+
+            return redirect()->route('homes.index')->with('error', 'Tài khoản hoặc mật khẩu không chính xác!');;
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        return redirect()->route('homes.index');
+    }
+}
