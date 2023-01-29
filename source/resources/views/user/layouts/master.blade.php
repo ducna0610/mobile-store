33047cd4050639ed111c9ac7c43ca51a081058c8
ducna0610 <ducna0610@gmail.com>
2023-01-29 09:23:00 +0700

    👍 update (add CheckUserLogin Middleware)

--- source/resources/views/user/layouts/master.blade.php
+++ source/resources/views/user/layouts/master.blade.php
@@ -59,6 +59,11 @@
     </footer>
     <!-- Footer -->
 
+    {{-- modal signing --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js"
+        integrity="sha512-EKWWs1ZcA2ZY9lbLISPz8aGR2+L7JVYqBAYTq5AXgBkSjRSuQEGqWx8R1zAX16KdXPaCjOCaKE8MCpU0wcHlHA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
