d540425ccbac75aa442fa0aae1b0251320965bc6
ducna0610 <ducna0610@gmail.com>
2023-01-29 08:31:04 +0700

    👍 update(add bootstrap): user

--- source/resources/views/user/layouts/master.blade.php
+++ source/resources/views/user/layouts/master.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8">
     <title>Mobile Store</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="{{ asset('user/css/bootstrap.min.css') }}">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Open+Sans">
 
@@ -59,10 +58,6 @@
         <!-- Copyright -->
     </footer>
     <!-- Footer -->
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js"
-        integrity="sha512-EKWWs1ZcA2ZY9lbLISPz8aGR2+L7JVYqBAYTq5AXgBkSjRSuQEGqWx8R1zAX16KdXPaCjOCaKE8MCpU0wcHlHA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
